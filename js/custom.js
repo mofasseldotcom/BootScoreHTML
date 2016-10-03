27312535049e00901704a761270506d76e94383a
webguymofassel <webguymofassel@gmail.com>
2016-10-03 18:31:18 +0600

    updated

--- js/custom.js
+++ js/custom.js
@@ -6,28 +6,41 @@
 
 	$('#portfolio-items').mixItUp();
      
-      var owl = $("#brand-carousel");
+  
+  var testimonial = $("#testimonial-carousel");
+ 
+  testimonial.owlCarousel({
+    singleItem : true, // Display only one item
+    navigation : false, // Show next and prev buttons
+    navigationText : ["&lt;","&gt;"],
+    pagination : true, //Show pagination
+    paginationSpeed : 1000, // Pagination speed in milliseconds
+    slideSpeed : 1000, // Slide speed in milliseconds
+    rewindSpeed : 1000, // Rewind speed in milliseconds
+    autoPlay : 5000, // Default speed will be 5 seconds.
+    stopOnHover : true, // Stop autoplay on mouse hover
+    
+  });
      
-      owl.owlCarousel({
-          items : 5, //10 items above 1000px browser width
-          itemsDesktop : [1000,5], //5 items between 1000px and 901px
-          itemsDesktopSmall : [900,3], // betweem 900px and 601px
-          itemsTablet: [600,2], //2 items between 600 and 0
-          itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
-      });
-     
-      // Custom Navigation Events
-      $(".next").click(function(){
-        owl.trigger('owl.next');
-      })
-      $(".prev").click(function(){
-        owl.trigger('owl.prev');
-      })
-      $(".play").click(function(){
-        owl.trigger('owl.play',1000); //owl.play event accept autoPlay speed as second parameter
-      })
-      $(".stop").click(function(){
-        owl.trigger('owl.stop');
-      })
+  var brand = $("#brand-carousel");
+ 
+  brand.owlCarousel({
+    singleItem : false, // Display only one item
+    navigation : true, // Show next and prev buttons
+    navigationText : ["&lt;","&gt;"],
+    pagination : false, //Show pagination
+    paginationSpeed : 1000, // Pagination speed in milliseconds
+    slideSpeed : 1000, // Slide speed in milliseconds
+    rewindSpeed : 1000, // Rewind speed in milliseconds
+    autoPlay : 1000, // Default speed will be 5 seconds.
+    stopOnHover : true, // Stop autoplay on mouse hover
+
+    items : 5, //10 items above 1000px browser width
+    itemsDesktop : [1000,5], //5 items between 1000px and 901px
+    itemsDesktopSmall : [900,3], // betweem 900px and 601px
+    itemsTablet: [600,2], //2 items between 600 and 0
+    itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
+    
+  });
 
 } )( jQuery );
